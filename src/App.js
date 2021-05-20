ff1e1fc39f329e135f43132f7340041ca0123a07
Tousif777 <tousifhossain.777@gmail.com>
2021-05-21 00:29:55 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import Body from "./components/Body";
-import Nav from "./components/Nav";
-import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-
-          <Route exact path="/contact" component={Footer} />
-
-          <Route exact path="/service" component={Body} />
-        </Switch>
-      </Router>
+      <h1>hi</h1>
     </>
   );
 }
